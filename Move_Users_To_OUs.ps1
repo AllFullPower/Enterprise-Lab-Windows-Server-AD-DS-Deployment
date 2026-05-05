5e8a38084ed00e92334721cfe07c4d0c626a358a
Jefferson Rojas <105081571+AllFullPower@users.noreply.github.com>
2026-05-04 22:01:57 -0600

    Add files via upload

---
+++ Move_Users_To_OUs.ps1
@@ -0,0 +1,28 @@
+﻿# Creating the mappings of Group name to Gruop's DistinguishedName
+$groupsToOUsMapping = @{
+    "IT_Department" = (Get-ADOrganizationalUnit -Filter * | Where-Object -Property "Name" -EQ "IT Department").DistinguishedName
+    "HR_Department" = (Get-ADOrganizationalUnit -Filter * | Where-Object -Property "Name" -EQ "HR Department").DistinguishedName
+    "Financial_Department" = (Get-ADOrganizationalUnit -Filter * | Where-Object -Property "Name" -EQ "Financial Department").DistinguishedName
+    "Customer_Service_Agents" = (Get-ADOrganizationalUnit -Filter * | Where-Object -Property "Name" -EQ "Customer Service Agents").DistinguishedName
+}
+
+
+# Entering into the first loop that would iterate through the 4 groups
+foreach ($group in $groupsToOUsMapping.Keys){
+    Write-Host "Getting members from group: $group"
+    
+    # Getting an array with the groups members of the selected group
+    $groupMemebers = Get-ADGroupMember -Identity $group
+
+    # Getting the user object on the and then using it to get its 
+    # DistinguishedName in order to use it and move the user from OU
+
+    foreach ($member in ($groupMemebers | Where-Object {$_.objectClass -eq "user"})){
+        
+        $memberPath = $member.distinguishedName
+        Move-ADObject $memberPath -TargetPath $groupsToOUsMapping[$group] # Moving the user
+
+        Write-Host "$member.name correctly moved to: $($groupsToOUsMapping[$group])"
+    }
+
+}
---
+++ Set_Random_Group_Script.ps1
@@ -0,0 +1,56 @@
+﻿$domainDistinguisedName = "$((Get-ADDomain).DistinguishedName)"
+
+
+$usaUsers = Get-ADUser -Filter * -SearchBase "OU=Users,OU=USA,$domainDistinguisedName"
+$usaGroups = Get-ADGroup -Filter * -SearchBase "OU=Groups,OU=USA,$domainDistinguisedName"
+
+
+$customerServiceAmount = ($usaUsers.Count * 0.55)
+$itAmount = ($usaUsers.Count * 0.20)
+$hrAmount = ($usaUsers.Count * 0.10)
+$financialAmount = ($usaUsers.Count * 0.15)
+
+
+Write-Host ($customerServiceAmount + $itAmount + $hrAmount + $financialAmount)
+Write-Host $customerServiceAmount $itAmount $hrAmount $financialAmount
+
+
+function SetRandomGroup {
+    param (
+        $users
+    )
+
+    foreach ( $user in $users ){
+        
+        if ( $customerServiceAmount -gt 0){
+
+            Add-ADGroupMember -Identity "Customer_Service_Agents" -Members $user.name
+            Write-Host "$($user.name) set in Customer Service" -BackgroundColor Black -ForegroundColor Cyan
+            $customerServiceAmount = $($customerServiceAmount - 1)
+            Write-Host "Available spaces $customerServiceAmount" 
+        }
+        elseif ($itAmount -gt 0){
+            Add-ADGroupMember -Identity "IT_Department" -Members $user.name
+            Write-Host "$($user.name) set in IT Department" -BackgroundColor Black -ForegroundColor Cyan
+            $itAmount = $($itAmount - 1)
+            Write-Host "Available spaces $itAmount"        
+        }
+        elseif ($hrAmount -gt 0){
+            Add-ADGroupMember -Identity "HR_Department" -Members $user.name
+            Write-Host "$($user.name) set in HR Department" -BackgroundColor Black -ForegroundColor Cyan
+            $hrAmount = $($hrAmount - 1)
+            Write-Host "Available spaces $hrAmount"        
+        }
+        elseif ($financialAmount -gt 0){
+            Add-ADGroupMember -Identity "Financial_Department" -Members $user.name
+            Write-Host "$($user.name) set in Financial Department" -BackgroundColor Black -ForegroundColor Cyan
+            $financialAmount = $($financialAmount - 1)
+            Write-Host "Available spaces $financialAmount"        
+        }
+    }
+
+
+}
+
+
+SetRandomGroup $usaUsers $usaGroups
